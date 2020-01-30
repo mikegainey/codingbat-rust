5a06ecd53881b18ae6fe36cf1888dcef3fa19b8b
Mike Gainey <mikeg9b@gmail.com>
2020-01-30 17:46:03 -0600

    array2 problems

--- src/array2.rs
+++ src/array2.rs
@@ -1,49 +1,141 @@
-// Array-2 > has22
-// https://codingbat.com/prob/p121853
+// Array-2 > countEvens
+// https://codingbat.com/prob/p162010
 
-//     Given an array of ints, return true if the array contains a 2 next to a 2 somewhere.
+// Return the number of even ints in the given array.
 
-//     has22([1, 2, 2]) → true
-//     has22([1, 2, 1, 2]) → false
-//     has22([2, 1, 2]) → false
+// count_evens([2, 1, 2, 3, 4]) → 3
+// count_evens([2, 2, 0]) → 3
+// count_evens([1, 3, 5]) → 0
 
-fn has22(a: &[i32]) -> bool {
-    let mut lastn = a[0];
-    for x in 1 .. a.len() {
-        if a[x] == 2 && lastn == 2 {
-            return true
-        }
-        lastn = a[x];
-    }
-    return false
+fn count_evens(array: &[i32]) -> i32 {
+    array.iter()
+        .filter(|&x| x % 2 == 0)
+        .count() as i32
 }
 
-// Array-2 > more14
-// https://codingbat.com/prob/p104627
+// Array-2 > sum13
+// https://codingbat.com/prob/p127384
 
-//     Given an array of ints, return true if the number of 1's is greater than the number of 4's
+// Return the sum of the numbers in the array, returning 0 for an empty array.
+// Except the number 13 is very unlucky, so it does not count and numbers that come immediately after a 13 also do not count.
 
-//     more14([1, 4, 1]) → true
-//     more14([1, 4, 1, 4]) → false
-//     more14([1, 1]) → true
-fn more14(a: &[u32]) -> bool {
+// sum13([1, 2, 2, 1]) → 6
+// sum13([1, 1]) → 2
+// sum13([1, 2, 2, 1, 13]) → 6
 
-    fn helper(a: &[u32], ones: u32, fours: u32) -> bool {
-        if a.len() == 0 {
-            return ones > fours
+fn sum13(array: &[i32]) -> i32 {
+    let mut count = 0;
+    let mut previous = None;
+    for &a in array {
+        if a == 13 {
+            previous = Some(13);
+            continue;
+        } else if previous == Some(13) {
+            previous = Some(a);
+            continue;
         } else {
-            let head = &a[0];
-            let tail = &a[1..];
-            if *head == 1 {
-                helper(tail, ones+1, fours)
-            } else if *head == 4 {
-                helper(tail, ones, fours+1)
-            } else {
-                helper(tail, ones, fours)
-            }
+            count += a;
+            previous = Some(a);
         }
     }
-    helper(&a, 0, 0) // trampoline with the input and accumulators initialized to zero
+    count
+}
+
+// Array-2 > lucky13
+// https://codingbat.com/prob/p194525
+
+// Given an array of ints, return true if the array contains no 1's and no 3's.
+
+// lucky13([0, 2, 4]) → true
+// lucky13([1, 2, 3]) → false
+// lucky13([1, 2, 4]) → false
+
+fn lucky13(array: &[i32]) -> bool {
+    array.iter()
+        .filter(|&x| x == &1 || x == &3)
+        .count() == 0
+}
+
+// Array-2 > fizzArray
+// https://codingbat.com/prob/p180920
+
+// Given a number n, create and return a new int array of length n, containing the numbers 0, 1, 2, ... n-1.
+// The given n may be 0, in which case just return a length 0 array.
+
+// fizz_array(4) → [0, 1, 2, 3]
+// fizz_array(1) → [0]
+// fizz_array(10) → [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+fn fizz_array1(n: u32) -> Vec<u32> {
+    (0..n).collect::<Vec<u32>>()
+}
+
+fn fizz_array2(n: u32) -> Vec<u32> {
+    let mut array = Vec::new();
+    for x in 0..n {
+        array.push(x);
+    }
+    array
+}
+
+// Array-2 > no14
+// https://codingbat.com/prob/p136648
+
+// Given an array of ints, return true if it contains no 1's or it contains no 4's.
+
+// no14([1, 2, 3]) → true
+// no14([1, 2, 3, 4]) → false
+// no14([2, 3, 4]) → true
+
+fn no14(array: &[i32]) -> bool {
+    let has_one = array.contains(&1);
+    let has_four = array.contains(&4);
+    !(has_one && has_four)
+}
+
+// Array-2 > matchUp
+// https://codingbat.com/prob/p136254
+
+// Given arrays nums1 and nums2 of the same length,
+// for every element in nums1, consider the corresponding element in nums2 (at the same index).
+// Return the count of the number of times that the two elements differ by 2 or less, but are not equal.
+
+// match_up([1, 2, 3], [2, 3, 10]) → 2
+// match_up([1, 2, 3], [2, 3, 5]) → 3
+// match_up([1, 2, 3], [2, 3, 3]) → 2
+
+fn match_up(num1: &[i32], num2: &[i32]) -> u32 {
+    num1.iter()
+        .zip(num2.iter())
+        .filter(|(&a, &b)| (a - b).abs() == 2 || (a - b).abs() == 1)
+        .count() as u32
+}
+
+// Array-2 > modThree
+// https://codingbat.com/prob/p159979
+
+// Given an array of ints, return true if the array contains either 3 even or 3 odd values all next to each other.
+
+// mod_three([2, 1, 3, 5]) → true
+// mod_three([2, 1, 2, 5]) → false
+// mod_three([2, 4, 2, 5]) → true
+
+fn mod_three(array: &[i32]) -> bool {
+
+    fn even(n: i32) -> bool { n % 2 == 0 }
+
+    for x in 0 ..= array.len()-3 {
+        // check for three consecutive even numbers
+        if even(array[x]) && even(array[x+1]) && even(array[x+2]) {
+            return true;
+        }
+        // check for three consecutive odd numbers
+        if array[x..x+3].iter() // just to see if this would work, ... and it did!
+            .all(|&x| x % 2 == 1) {
+                return true;
+            }
+    }
+    false
 }
 
 #[cfg(test)]
@@ -51,17 +143,62 @@ mod tests {
     use super::*;
 
     #[test]
-    fn has22_test() {
-        assert!(has22(&[1, 2, 2]) == true);
-        assert!(has22(&[1, 2, 1, 2]) == false);
-        assert!(has22(&[2, 1, 2]) == false);
+    fn count_evens_test() {
+        assert_eq!(count_evens(&[2, 1, 2, 3, 4]), 3);
+        assert_eq!(count_evens(&[2, 2, 0]), 3);
+        assert_eq!(count_evens(&[1, 3, 5]), 0);
     }
 
     #[test]
-    fn more14_test() {
-        assert!(more14(&[1, 4, 1]) == true);
-        assert!(more14(&[1, 4, 1, 4]) == false);
-        assert!(more14(&[1, 1]) == true);
+    fn sum13_test() {
+        assert_eq!(sum13(&[1, 2, 2, 1]), 6);
+        assert_eq!(sum13(&[1, 1]), 2);
+        assert_eq!(sum13(&[1, 2, 2, 1, 13]), 6);
+        assert_eq!(sum13(&[1, 2, 2, 1, 13, 56]), 6);
+        assert_eq!(sum13(&[13, 2, 3, 1, 3]), 7);
     }
 
+    #[test]
+    fn lucky13_test() {
+        assert_eq!(lucky13(&[0, 2, 4]), true);
+        assert_eq!(lucky13(&[1, 2, 3]), false);
+        assert_eq!(lucky13(&[1, 2, 4]), false);
+    }
+
+    #[test]
+    fn fizz_array1_test() {
+        assert_eq!(fizz_array1(4), [0, 1, 2, 3]);
+        assert_eq!(fizz_array1(1), [0]);
+        assert_eq!(fizz_array1(10), [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
+    }
+
+    #[test]
+    fn fizz_array2_test() {
+        assert_eq!(fizz_array2(4), [0, 1, 2, 3]);
+        assert_eq!(fizz_array2(1), [0]);
+        assert_eq!(fizz_array2(10), [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
+    }
+
+    #[test]
+    fn no14_test() {
+        assert_eq!(no14(&[1, 2, 3]), true);
+        assert_eq!(no14(&[1, 2, 3, 4]), false);
+        assert_eq!(no14(&[2, 3, 4]), true);
+    }
+
+    #[test]
+    fn match_up_test() {
+        assert_eq!(match_up(&[1, 2, 3], &[2, 3, 10]), 2);
+        assert_eq!(match_up(&[1, 2, 3], &[2, 3, 5]), 3);
+        assert_eq!(match_up(&[1, 2, 3], &[2, 3, 3]), 2);
+        assert_eq!(match_up(&[1, 2, 3, 4], &[5, 4, 3, 2]), 2);
+    }
+
+    #[test]
+    fn mod_three_test() {
+        assert_eq!(mod_three(&[2, 1, 3, 5]), true);
+        assert_eq!(mod_three(&[2, 1, 2, 5]), false);
+        assert_eq!(mod_three(&[2, 4, 2, 5]), true);
+        assert_eq!(mod_three(&[2, 4, 7, 5, 1]), true);
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -5,3 +5,65 @@
 
 fn main() {
 }
+
+// Array-2 > has22
+// https://codingbat.com/prob/p121853
+
+//     Given an array of ints, return true if the array contains a 2 next to a 2 somewhere.
+
+//     has22([1, 2, 2]) → true
+//     has22([1, 2, 1, 2]) → false
+//     has22([2, 1, 2]) → false
+
+fn has22(a: &[i32]) -> bool {
+    let mut lastn = a[0];
+    for x in 1 .. a.len() {
+        if a[x] == 2 && lastn == 2 {
+            return true
+        }
+        lastn = a[x];
+    }
+    return false
+}
+
+// Array-2 > more14
+// https://codingbat.com/prob/p104627
+
+//     Given an array of ints, return true if the number of 1's is greater than the number of 4's
+
+//     more14([1, 4, 1]) → true
+//     more14([1, 4, 1, 4]) → false
+//     more14([1, 1]) → true
+fn more14(a: &[u32]) -> bool {
+
+    fn helper(a: &[u32], ones: u32, fours: u32) -> bool {
+        if a.len() == 0 {
+            return ones > fours
+        } else {
+            let head = &a[0];
+            let tail = &a[1..];
+            if *head == 1 {
+                helper(tail, ones+1, fours)
+            } else if *head == 4 {
+                helper(tail, ones, fours+1)
+            } else {
+                helper(tail, ones, fours)
+            }
+        }
+    }
+    helper(&a, 0, 0) // trampoline with the input and accumulators initialized to zero
+}
+
+// #[test]
+// fn has22_test() {
+//     assert!(has22(&[1, 2, 2]) == true);
+//     assert!(has22(&[1, 2, 1, 2]) == false);
+//     assert!(has22(&[2, 1, 2]) == false);
+// }
+
+// #[test]
+// fn more14_test() {
+//     assert!(more14(&[1, 4, 1]) == true);
+//     assert!(more14(&[1, 4, 1, 4]) == false);
+//     assert!(more14(&[1, 1]) == true);
+// }
