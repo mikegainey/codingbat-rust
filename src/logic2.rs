caf888c3ba9240249f48f994eb92fc0d2d0594a5
Mike Gainey <mikeg9b@gmail.com>
2020-01-29 19:58:53 -0600

    logic2 problems

--- src/logic2.rs
+++ src/logic2.rs
@@ -57,7 +57,68 @@ fn no_teen_sum(a: i32, b: i32, c: i32) -> i32 {
 // blackjack(19, 22) → 19
 
 fn blackjack(a: u32, b: u32) -> u32 {
-    a + b
+    use std::cmp;
+
+    if a > 21 && b > 21 {
+        0
+    } else if a > 21 {
+        b
+    } else if b > 21 {
+        a
+    } else {
+        cmp::max(a, b)
+    }
+}
+
+// Logic-2 > loneSum
+// https://codingbat.com/prob/p148972
+
+// Given 3 int values, a b c, return their sum.
+// However, if one of the values is the same as another of the values, it does not count towards the sum.
+
+// lone_sum(1, 2, 3) → 6
+// lone_sum(3, 2, 3) → 2
+// lone_sum(3, 3, 3) → 0
+
+fn lone_sum(a: i32, b: i32, c: i32) -> i32 {
+    if a == b && b == c {
+        0
+    } else if a == b {
+        c
+    } else if b == c {
+        a
+    } else if a == c {
+        b
+    } else {
+        a + b + c
+    }
+}
+
+// Logic-2 > roundSum
+// https://codingbat.com/prob/p186753
+
+// For this problem, we'll round an int value up to the next multiple of 10 if its rightmost digit is 5 or more,
+// so 15 rounds up to 20. Alternately, round down to the previous multiple of 10 if its rightmost digit is less than 5,
+// so 12 rounds down to 10.
+// Given 3 ints, a b c, return the sum of their rounded values.
+// To avoid code repetition, write a separate helper "public int round10(int num) {" and call it 3 times.
+
+// round_sum(16, 17, 18) → 60
+// round_sum(12, 13, 14) → 30
+// round_sum(6, 4, 4) → 10
+
+fn round_sum(a: i32, b: i32, c: i32) -> i32 {
+
+    fn round10(n: i32) -> i32 {
+        let n_mod10 = n % 10;
+        if n_mod10 >= 5 {
+            n + (10 - n_mod10)
+        } else {
+            n - n_mod10
+        }
+    }
+
+    round10(a) + round10(b) + round10(c)
 }
 
 #[cfg(test)]
@@ -82,6 +143,24 @@ mod tests {
     fn blackjack_test() {
         assert_eq!(blackjack(19, 21), 21);
         assert_eq!(blackjack(21, 19), 21);
-        assert_eq!(blackjack(19, 22), 19);
+        assert_eq!(blackjack(9, 22), 9);
+        assert_eq!(blackjack(22, 1), 1);
+        assert_eq!(blackjack(23, 24), 0);
+    }
+
+    #[test]
+    fn lone_sum_test() {
+        assert_eq!(lone_sum(1, 2, 3), 6);
+        assert_eq!(lone_sum(3, 2, 3), 2);
+        assert_eq!(lone_sum(3, 3, 3), 0);
+        assert_eq!(lone_sum(3, 3, 4), 4);
+        assert_eq!(lone_sum(3, 4, 4), 3);
+    }
+
+    #[test]
+    fn round_sum_test() {
+        assert_eq!(round_sum(16, 17, 18), 60);
+        assert_eq!(round_sum(12, 13, 14), 30);
+        assert_eq!(round_sum(6, 4, 4), 10);
     }
 }
